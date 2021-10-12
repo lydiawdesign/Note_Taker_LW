44233514c2c6e4324d5e5c6ab2314131e7040c30
lydiawdesign <lydia.williamson.OSU@gmail.com>
2021-10-12 13:07:21 -0400

    screenshot added

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.post("/api/notes", (req, res) => {
     res.json(notes);
 })
 
-// `DELETE /api/notes/:id` should receive a query parameter that contains the id of a note to delete. To delete a note, you"ll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
+// BONUS `DELETE /api/notes/:id` should receive a query parameter that contains the id of a note to delete. To delete a note, you"ll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
 
 app.listen(PORT, () =>
   console.log(`Note Taker App is listening at http://localhost:${PORT}`)
