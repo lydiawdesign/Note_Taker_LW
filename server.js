f11f982885d97fa76c965a81725165772c5b484f
lydiawdesign <lydia.williamson.OSU@gmail.com>
2021-10-12 12:23:54 -0400

    troubleshooting

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // dependencies
 const express = require('express');
 const fs = require('fs');
-const db = require('./Develop/db/db.json');
+// const db = require('./Develop/db/db.json');
 const uuid = require('uuid');
 const path = require('path');
 
@@ -31,8 +31,8 @@ app.get('/api/notes', (req, res) => {
 // `POST /api/notes` should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
 app.post('/api/notes', (req, res) => {
     const notes = JSON.parse(fs.readFileSync('./db/db.json'))
-    const newNote = req.body;
-    newNote.id = uuid();
+    const newNotes = req.body;
+    newNotes.id = uuid();
     notes.push(newNotes);
     fs.writeFileSync('./db/db.json', JSON.stringify(notes))
     res.json(notes);
