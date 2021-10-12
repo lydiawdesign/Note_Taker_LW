e8be01fb5fa3b2a38fa47309c97de475b1d909d7
lydiawdesign <lydia.williamson.OSU@gmail.com>
2021-10-12 13:04:07 -0400

    restructured directories for troubleshooting

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 // dependencies
-const express = require('express');
-const fs = require('fs');
-// const db = require('./Develop/db/db.json');
-const uuid = require('uuid');
-const path = require('path');
+const express = require("express");
+const fs = require("fs");
+const db = require("./db/db.json");
+const uuid = require("uuid");
+const path = require("path");
 
 // set up express app
 const app = express();
@@ -11,34 +11,34 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // `GET *` should return the `index.html` file.
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, './Develop/public/index.html'))
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "./public/index.html"))
 );
 
 // `GET /notes` should return the `notes.html` file
-app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, './Develop/public/notes.html'))
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "./public/notes.html"))
 );
 
 // GET /api/notes` should read the `db.json` file and return all saved notes as JSON.
-app.get('/api/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './db/db.json'));
+app.get("/api/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "./db/db.json"));
 });
 
-// `POST /api/notes` should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
-app.post('/api/notes', (req, res) => {
-    const notes = JSON.parse(fs.readFileSync('./db/db.json'))
+// `POST /api/notes` should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client. You"ll need to find a way to give each note a unique id when it"s saved (look into npm packages that could do this for you).
+app.post("/api/notes", (req, res) => {
+    const notes = JSON.parse(fs.readFileSync("./db/db.json"))
     const newNotes = req.body;
-    newNotes.id = uuid();
+    newNotes.id = uuid.v4();
     notes.push(newNotes);
-    fs.writeFileSync('./db/db.json', JSON.stringify(notes))
+    fs.writeFileSync("./db/db.json", JSON.stringify(notes))
     res.json(notes);
 })
 
-// `DELETE /api/notes/:id` should receive a query parameter that contains the id of a note to delete. To delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
+// `DELETE /api/notes/:id` should receive a query parameter that contains the id of a note to delete. To delete a note, you"ll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
 
 app.listen(PORT, () =>
   console.log(`Note Taker App is listening at http://localhost:${PORT}`)
