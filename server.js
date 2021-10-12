182dc71383ef69ef17c218e0eb16fedcf4923866
lydiawdesign <lydia.williamson.OSU@gmail.com>
2021-10-12 09:18:54 -0400

    fiexed path for the get index and notes html

--- server.js
+++ server.js
@@ -2,12 +2,12 @@
 const express = require('express');
 const fs = require('fs');
 const db = require('./Develop/db/db.json');
-const uuid = require('./helpers/uuid');
+const uuid = require('uuid');
 const path = require('path');
 
 // set up express app
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -15,12 +15,12 @@ app.use(express.static('public'));
 
 // `GET *` should return the `index.html` file.
 app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, './public/index.html'))
+  res.sendFile(path.join(__dirname, './Develop/public/index.html'))
 );
 
 // `GET /notes` should return the `notes.html` file
 app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, './public/notes.html'))
+  res.sendFile(path.join(__dirname, './Develop/public/notes.html'))
 );
 
 // GET /api/notes` should read the `db.json` file and return all saved notes as JSON.
@@ -41,5 +41,5 @@ app.post('/api/notes', (req, res) => {
 // `DELETE /api/notes/:id` should receive a query parameter that contains the id of a note to delete. To delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
 
 app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT}`)
+  console.log(`Note Taker App is listening at http://localhost:${PORT}`)
 );
