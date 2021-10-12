13fa4e5eab2c33b95656eeffd01252075fc20679
lydiawdesign <lydia.williamson.OSU@gmail.com>
2021-10-11 20:23:54 -0400

    added dependencies, set up express app, and started setting up route in server.js

--- server.js
+++ server.js
@@ -1,5 +1,24 @@
+// dependencies
 const express = require("express");
 const fs = require("fs");
 const db = require("./Develop/db/db.json");
+const uuid = require('./helpers/uuid');
+const path = require('path');
 
+// set up express app
 const app = express();
+const PORT = 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+// setting up routes
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
